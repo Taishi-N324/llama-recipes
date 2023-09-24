92fe0355d7111a06ce9329d9ba7477712cb466f9
kazuki <kaz.tokyo.tech20@gmail.com>
2023-09-24 16:24:13 +0900

    fix: fix typo

--- src/llama_recipes/configs/training.py
+++ src/llama_recipes/configs/training.py
@@ -34,6 +34,6 @@ class train_config:
     dist_checkpoint_root_folder: str = "PATH/to/save/FSDP/model"  # will be used if using FSDP
     dist_checkpoint_folder: str = "fine-tuned"  # will be used if using FSDP
     save_optimizer: bool = False  # will be used if using FSDP
-    use_fast_kernels: bool = False  # Enable using SDPA from PyTroch Accelerated Transformers, make use Flash Attention and Xformer memory-efficient kernels
+    use_fast_kernels: bool = False  # Enable using SDPA from PyTorch Accelerated Transformers, make use Flash Attention and Xformer memory-efficient kernels
     use_mpi: bool = False
     wandb_name: Optional[str] = None
