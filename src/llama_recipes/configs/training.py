81219664bbe96ae04d0061efbc84aacc01bf58fb
Taishi-N324 <taishiahiru5@gmail.com>
2023-10-08 22:26:57 +0900

    Convenient settings for real training

--- src/llama_recipes/configs/training.py
+++ src/llama_recipes/configs/training.py
@@ -7,15 +7,15 @@ from typing import Optional
 
 @dataclass
 class train_config:
-    model_name: str = "/home/acf15834ji/.cache/huggingface/hub/models--meta-llama--Llama-2-13b-hf/snapshots/db6b8eb1feabb38985fdf785a89895959e944936"
-    tokenizer_name: str = "/home/acf15834ji/.cache/huggingface/hub/models--meta-llama--Llama-2-13b-hf/snapshots/db6b8eb1feabb38985fdf785a89895959e944936"
+    model_name: str = ""
+    tokenizer_name: str = ""
     enable_fsdp: bool = False
     low_cpu_fsdp: bool = False
     run_validation: bool = True
     batch_size_training: int = 4
     gradient_accumulation_steps: int = 1
     clip_grad_norm: float = 1.0
-    num_epochs: int = 10
+    num_epochs: int = 1
     num_workers_dataloader: int = 1
     lr: float = 1e-4
     lr_min: float = 1e-5
@@ -34,7 +34,7 @@ class train_config:
     use_fp16: bool = False
     mixed_precision: bool = True
     val_batch_size: int = 1
-    dataset: str = "ja_wikipedia_dataset"
+    dataset: str = ""
     peft_method: str = "lora"  # None , llama_adapter, prefix
     use_peft: bool = False
     output_dir: str = "PATH/to/save/PEFT/model"
@@ -50,6 +50,6 @@ class train_config:
     use_fast_kernels: bool = True  # Enable using SDPA from PyTorch Accelerated Transformers, make use Flash Attention and Xformer memory-efficient kernels
     use_mpi: bool = False
     use_streaming_datasets: bool = True
-    streaming_datasets_train_path: str = "/bb/llm/gaf51275/llama/datasets/llama2-llm-jp-corpus/v1.0.2/sample/streaming_ja/ja_wiki_no_zstd/train/"
-    streaming_datasets_val_path: str = "/bb/llm/gaf51275/llama/taishi-work-streaming/ABCI-llama-recipes/tools/data_prep/samsum2"
+    streaming_datasets_train_path: str = ""
+    streaming_datasets_val_path: str = "/bb/llm/gaf51275/llama/taishi-work-streaming/ABCI-llama-recipes/tools/data_prep/samsum2/"
     wandb_name: Optional[str] = None
