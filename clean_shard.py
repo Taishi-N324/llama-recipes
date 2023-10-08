583f0e246f212a66d8d74f8123401877c097f2fa
Taishi-N324 <taishiahiru5@gmail.com>
2023-10-08 19:42:43 +0900

    fixed the warning message because it seemed to cause misunderstanding.

--- clean_shard.py
+++ clean_shard.py
@@ -4,7 +4,7 @@ def clean_stale_memory():
     try:
         streaming.base.util.clean_stale_shared_memory()
     except FileNotFoundError as e:
-        print(f"Warning: {e}")
+        pass
 
 if __name__ == "__main__":
     clean_stale_memory()
